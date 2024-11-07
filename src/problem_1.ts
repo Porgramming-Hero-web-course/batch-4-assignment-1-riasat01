903217c581411619d81db7f3d5193434c720f3f2
riasat01 <riasat01@outlook.com>
2024-11-07 18:54:13 +0600

    problem 1 solved with and file contains function call and console log

---
+++ src/problem_1.ts
@@ -0,0 +1,11 @@
+{
+    const sumArray = (nums: number[]): number => {
+        return nums.reduce((tot: number, crnt: number) => tot + crnt, 0)
+    }
+    
+    // Sample Input:
+    const x: number = sumArray([1, 2, 3, 4, 5]);
+    console.log(x);
+    // Sample Output:
+    // 15;
+}
