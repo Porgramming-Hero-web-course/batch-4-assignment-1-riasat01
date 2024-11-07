fc7930362adc3d2c74d2f2f4dea432c696908e83
riasat01 <riasat01@outlook.com>
2024-11-07 21:31:06 +0600

    problem 7 done and file has function call and console log

---
+++ src/problem_7.ts
@@ -0,0 +1,15 @@
+{
+    class Car {
+        constructor( public make: string, public model: string, public year: number){};
+        getCarAge() {
+            const date = new Date();
+            return date.getFullYear() - this.year;
+        }
+    }
+    // Sample Input 1:
+    const car = new Car("Honda", "Civic", 2008);
+    const x: number = car.getCarAge();
+    console.log(x);
+    // Sample Output 1:
+    // 6(assuming current year is 2024)
+}
