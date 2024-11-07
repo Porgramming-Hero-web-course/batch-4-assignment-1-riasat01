693e884102fd9e13cf9b9ae7d4b593824e63ff14
riasat01 <riasat01@outlook.com>
2024-11-07 19:45:59 +0600

    problem 4 done and file has function call and console log

---
+++ src/problem_4.ts
@@ -0,0 +1,37 @@
+{
+    type Circle = {
+        shape: `circle`;
+        radius: number
+    }
+
+    type Rectangle = {
+        shape: `rectangle`;
+        width: number;
+        height: number
+    }
+
+    type ShapeType = Circle | Rectangle;
+
+    const calculateShapeArea = (shapeParam: ShapeType) : number | undefined => {
+        if(shapeParam.shape === `circle`){
+            return parseFloat((Math.PI * shapeParam.radius * shapeParam.radius).toPrecision(4));
+        }else if(shapeParam.shape === `rectangle`){
+            return shapeParam.width * shapeParam.height;
+        }
+    }
+    // Sample Input 1:
+    const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+    console.log(circleArea);
+    // Sample Output 1:
+    // 78.54;
+
+    // Sample Input 2:
+    const rectangleArea = calculateShapeArea({
+        shape: "rectangle",
+        width: 4,
+        height: 6,
+    });
+    console.log(rectangleArea);
+    // Sample Output 2:
+    // 24;
+}
