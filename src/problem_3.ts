86bee1019c140a0b11f08a101dfc1c408c979ce6
riasat01 <riasat01@outlook.com>
2024-11-07 19:30:11 +0600

    problem 3 done and file has function call and console log

---
+++ src/problem_3.ts
@@ -0,0 +1,17 @@
+{
+    const countWordOccurrences = (sentence: string, word: string) : number => {
+        let wordCounter : number = 0;
+        const wordArray : string[] = sentence.toLowerCase().split(' ');
+        for (const wd of wordArray){
+            if(wd.includes(word)){
+                ++wordCounter;
+            }
+        }
+        return wordCounter;
+    }
+    // Sample Input:
+    const x : number = countWordOccurrences("I lotypescRipTve typescript", "typescript");
+    console.log(x);
+    // Sample Output:
+    // 1;
+}
