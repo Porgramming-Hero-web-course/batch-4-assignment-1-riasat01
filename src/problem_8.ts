090d041a8a068ca89f079e2cd209fe2d268f8616
riasat01 <riasat01@outlook.com>
2024-11-08 14:22:07 +0600

    problem 8 done with funtion invoking and console log

---
+++ src/problem_8.ts
@@ -0,0 +1,16 @@
+{
+    function validateKeys<T>(obj: T, keys: (keyof T)[]){
+        for(const key of keys){
+            if(typeof obj === `object` && obj !==null && !(key in obj)){
+                return false;
+            }
+        }
+        return true;
+    } 
+    // Sample Input:
+    const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    console.log(validateKeys(person, ["name", "age"]));
+
+    // Sample Output:
+    // true;
+}
